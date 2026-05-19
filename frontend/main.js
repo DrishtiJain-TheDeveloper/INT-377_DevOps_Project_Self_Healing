abb1d65708d1c8ae66c6aaf878bb438d2b41e600
Drishti Jain <drishtijain882@gmail.com>
2026-05-19 15:47:44 +0530

    updated

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://backend-service:5000';
+const API_BASE = 'http://34.xxx.xxx.xxx:30001';
 
 const facts = [
   "The ocean covers more than 70% of Earth's surface but over 80% of it remains unexplored.",
